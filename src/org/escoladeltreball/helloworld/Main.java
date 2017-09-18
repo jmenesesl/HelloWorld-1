248588611cd4002dd1c65780e7b2f5a452d5d9e9
Pep Mendez <jmendez1@xtec.cat>
2017-09-18 17:51:17 +0200

    First commit

---
+++ src/org/escoladeltreball/helloworld/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.escoladeltreball.helloworld;
+
+/**
+ * @author jmendez
+ *
+ */
+public final class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
